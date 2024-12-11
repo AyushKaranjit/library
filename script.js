49bfb125e250a6db8ddd93f362f2309e64426c14
Ayush Karanjit <aayushkaranjit2@gmail.com>
2024-12-11 14:25:15 +0545

    made libray system wiith objects

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  const tableBody = document.querySelector("#libraryTable tbody");
+  tableBody.innerHTML = "";
+  myLibrary.forEach((book) => {
+    const row = document.createElement("tr");
+
+    Object.values(book).forEach((value) => {
+      const cell = document.createElement("td");
+      cell.textContent = value;
+      row.appendChild(cell);
+    });
+
+    tableBody.appendChild(row);
+  });
 }
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, false);
+addBookToLibrary("The Lord of the Rings", "J.R.R. Tolkien", 1178, true);
+addBookToLibrary("The Silmarillion", "J.R.R. Tolkien", 365, false);
+addBookToLibrary("The Children of Húrin", "J.R.R. Tolkien", 313, true);
+
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + button");
+const closeButton = document.querySelector("dialog button");
+
+// "Show the dialog" button opens the dialog modally
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+// "Close" button closes the dialog
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
